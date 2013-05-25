85d50852f8981e1d8850a39abf4d7c4eb3817cec
Zaccheo <zaccheo@cucciolo.(none)>
2013-05-25 14:13:43 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='RESTnGo',
+	description="Library for building REST service",
+	author="Bagnati Zaccheo",
+	author_email="zaccheob@gmail.com",
+	version='0.1',
+	py_modules=['restngo']
+)
